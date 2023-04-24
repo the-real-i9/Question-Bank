78d164ea4532e3479a27b084af297bda52f18992
Mckenney <oluwarinolasam@gmail.com>
2023-04-24 08:22:52 +0100

    updated styling

--- src/components/QuestionAnswer.jsx
+++ src/components/QuestionAnswer.jsx
@@ -46,13 +46,13 @@ function QuestionAnswer({
             type="button"
             className={`toggle-answer-visibility mr-3 text-sm ${
               answerShow ? "bg-slate-900" : "bg-white"
-            } text-prim-500 px-4 py-1 rounded-full border border-slate-200`}
+            } text-prim-500 px-4 py-1 rounded-full border`}
           >
             {answerShow ? "Hide" : "Seek"}
           </button>
           <button
             type="button"
-            className={`w-6 h-6 p-1 bg-clip-content border border-slate-300 rounded-full ${
+            className={`w-6 h-6 p-1 bg-clip-content border rounded-full ${
               questionCheck ? "bg-prim-600" : "bg-transparent"
             }`}
             id={qid}
